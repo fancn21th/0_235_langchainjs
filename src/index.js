e159fde87091bb84de4080961fc5eeea755b92f0
fancn21th <fancn21th@gmail.com>
2024-05-19 19:13:04 +0800

    📦 NEW: behind proxy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import "dotenv/config";
-
+import { HttpsProxyAgent } from "https-proxy-agent";
 import { ChatOpenAI } from "@langchain/openai";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
-const model = new ChatOpenAI({});
+// Configure your corporate proxy settings
+const proxyUrl = process.env.PROXY_URL; // PROXY_URL=http://localhost:1234 defined in .env file
+const proxyAgent = new HttpsProxyAgent(proxyUrl);
+
+const model = new ChatOpenAI({
+  configuration: {
+    httpAgent: proxyAgent, // Use httpAgent or httpsAgent based on your proxy protocol
+  },
+});
 const promptTemplate = PromptTemplate.fromTemplate(
   "Tell me a joke about {topic}"
 );
